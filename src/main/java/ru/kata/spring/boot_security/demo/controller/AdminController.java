7694b855edfcb4500fc03e5e02a79d7e01f70697
Viktor Sukmanov <vicktor.sukmanov@yandex.ru>
2023-11-14 12:43:21 +0300

    Добавил валидацию

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -40,7 +42,12 @@ public class AdminController {
     }
 
     @PostMapping("/admin")
-    public String createUser(@ModelAttribute("user") User user) {
+    public String createUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model) {
+       if (bindingResult.hasErrors()){
+           List<Role> roles =  roleRepository.findAll();
+           model.addAttribute("roles", roles);
+           return "new";
+       }
         userService.createUser(user);
         return "redirect:/admin";
     }
@@ -55,7 +62,12 @@ public class AdminController {
     }
 
     @PostMapping("admin/{id}")
-    public String updateUser(Model model, @ModelAttribute("user") User user) {
+    public String updateUser(Model model, @ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            List<Role> roles =  roleRepository.findAll();
+            model.addAttribute("roles", roles);
+            return "edit";
+        }
         userService.updateUser(user);
         return "redirect:/admin";
     }
--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -7,6 +7,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Set;
 
@@ -17,15 +20,20 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     @Column(name = "username")
     private String username;
 
     @Column(name = "last_name")
     private String lastName;
-
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     @Column(name = "email")
     private String email;
+
+//    @NotEmpty(message = "Password should not be empty")
+//    @Size(min = 2, max = 30, message = "Password should be between 2 and 30 characters")
     @Column(name = "password")
     private String password;
     @Fetch(FetchMode.SUBSELECT)
