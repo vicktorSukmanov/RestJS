87a92fa87ed0751253e1e0f7115cdd7c66bc83fd
Viktor Sukmanov <vicktor.sukmanov@yandex.ru>
2023-11-18 16:35:39 +0300

    RestController

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -22,9 +22,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/", "/index","/api/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
+                //.antMatchers("/api/**").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
                 .and()
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -0,0 +1,58 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.hibernate.annotations.Any;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class AdminRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUser(){
+        return new ResponseEntity<>(userService.getListUser(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") int id){
+        return new ResponseEntity<>(userService.readUser(id),HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> getAuthUser(Principal principal){
+        User user =(User) userService.loadUserByUsername(principal.getName());
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<HttpStatus> addUser(@ResponseBody User user){
+        userService.createUser(user);
+        return new  ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @PatchMapping("/user")
+    public ResponseEntity<HttpStatus> updateUser(@ResponseBody User user){
+        userService.updateUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") int id ){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+}
